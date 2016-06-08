2f605d1aedec0ef58e68df3b7455ec76c7db1be8
inventivetalent <inventivetalent@inventivegames.de>
2016-06-09 00:02:13 +0200

    remove type arguments (Let's hope I was just tired and they had no further purpose...)

--- API/src/main/java/org/inventivetalent/npclib/npc/living/NPCArmorStand.java
+++ API/src/main/java/org/inventivetalent/npclib/npc/living/NPCArmorStand.java
@@ -2,18 +2,16 @@ package org.inventivetalent.npclib.npc.living;
 
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.inventivetalent.npclib.annotation.NPC;
 import org.inventivetalent.npclib.entity.living.EntityArmorStand;
-import org.inventivetalent.npclib.entity.living.NPCEntityLiving;
 
 @NPC(id = 30,
 	 type = EntityType.ARMOR_STAND,
 	 bukkit = ArmorStand.class,
 	 nms = "EntityArmorStand",
 	 entity = EntityArmorStand.class)
-public class NPCArmorStand<N extends NPCEntityLiving, B extends LivingEntity> extends NPCLivingAbstract<N, B> {
-	protected NPCArmorStand(N npcEntity) {
+public class NPCArmorStand extends NPCLivingAbstract<EntityArmorStand, ArmorStand> {
+	protected NPCArmorStand(EntityArmorStand npcEntity) {
 		super(npcEntity);
 	}
 }
