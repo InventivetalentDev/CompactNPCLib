7abf94dd2d9d1db018c7789af21eb4f25c719c90
inventivetalent <inventivetalent@inventivegames.de>
2016-06-12 22:07:34 +0200

    init the API

--- Plugin/src/main/java/org/inventivetalent/npclib/NPCLibPlugin.java
+++ Plugin/src/main/java/org/inventivetalent/npclib/NPCLibPlugin.java
@@ -6,20 +6,35 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.inventivetalent.apihelper.APIManager;
 import org.inventivetalent.npclib.command.SpawnCommands;
 import org.inventivetalent.npclib.registry.NPCRegistry;
 import org.inventivetalent.pluginannotations.PluginAnnotations;
 
 public class NPCLibPlugin extends JavaPlugin implements Listener {
 
+	private NPCLib npcLibInstance = new NPCLib();
 	private NPCRegistry pluginNpcRegistry;
+
+	@Override
+	public void onLoad() {
+		APIManager.registerAPI(npcLibInstance);
+	}
 
 	@Override
 	public void onEnable() {
 		NPCLib.logger = getLogger();
+
+		APIManager.initAPI(NPCLib.class);
+		pluginNpcRegistry = NPCLib.createRegistry(this);
+
 		Bukkit.getPluginManager().registerEvents(this, this);
 		PluginAnnotations.COMMAND.load(this, new SpawnCommands(this));
-		pluginNpcRegistry = NPCLib.createRegistry(this);
+	}
+
+	@Override
+	public void onDisable() {
+		APIManager.disableAPI(NPCLib.class);
 	}
 
 	public NPCRegistry getPluginNpcRegistry() {
