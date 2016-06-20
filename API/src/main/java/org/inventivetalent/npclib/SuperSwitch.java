f24e7efa9eff6c3af96e167dee46ca58d0880869
inventivetalent <inventivetalent@inventivegames.de>
2016-06-20 23:01:59 +0200

    add SuperSwitch

---
+++ API/src/main/java/org/inventivetalent/npclib/SuperSwitch.java
@@ -0,0 +1,51 @@
+package org.inventivetalent.npclib;
+
+import lombok.*;
+import org.bukkit.event.Cancellable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class SuperSwitch implements Cancellable {
+
+	private State state = State.PASS;
+
+	@Override
+	public boolean isCancelled() {
+		return this.state == State.CANCEL;
+	}
+
+	@Override
+	public void setCancelled(boolean b) {
+		this.state = State.CANCEL;
+	}
+
+	public boolean callSuper() {
+		return this.state == State.PASS || this.state == State.REPLACE;
+	}
+
+	public boolean isReplace() {
+		return this.state == State.REPLACE;
+	}
+
+	public static SuperSwitch newInstance() {
+		return new SuperSwitch();
+	}
+
+	public enum State {
+		/**
+		 * Let the call pass and return the default value by calling the super method
+		 */
+		PASS,
+		/**
+		 * Cancel the call entirely and don't call super
+		 */
+		CANCEL,
+		/**
+		 * Call super, but replace the returned value of the method
+		 */
+		REPLACE;
+	}
+}
