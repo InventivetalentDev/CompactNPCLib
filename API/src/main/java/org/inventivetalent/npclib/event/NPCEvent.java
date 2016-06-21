67c622d3da047a91f0b822b5966327f9fb6fda9f
inventivetalent <inventivetalent@inventivegames.de>
2016-06-21 14:44:02 +0200

    add javadoc for NPCEvent

--- API/src/main/java/org/inventivetalent/npclib/event/NPCEvent.java
+++ API/src/main/java/org/inventivetalent/npclib/event/NPCEvent.java
@@ -5,6 +5,9 @@ import org.bukkit.event.Event;
 import org.inventivetalent.npclib.NPCType;
 import org.inventivetalent.npclib.npc.NPCAbstract;
 
+/**
+ * Abstract base class for NPC events
+ */
 public abstract class NPCEvent extends Event {
 
 	private final NPCAbstract npc;
@@ -13,14 +16,29 @@ public abstract class NPCEvent extends Event {
 		this.npc = npc;
 	}
 
+	/**
+	 * Get the involved NPC
+	 *
+	 * @return the NPC
+	 */
 	public NPCAbstract getNpc() {
 		return npc;
 	}
 
+	/**
+	 * Get the NPC's type
+	 *
+	 * @return the {@link NPCType}
+	 */
 	public NPCType getNpcType() {
 		return npc.getNpcType();
 	}
 
+	/**
+	 * Get the NPC's entity type
+	 *
+	 * @return the {@link EntityType}
+	 */
 	public EntityType getEntityType() {
 		return npc.getEntityType();
 	}
