0b7014fa2716d96f5a58253ccd782bb3c5b8a7c0
inventivetalent <inventivetalent@inventivegames.de>
2016-06-30 16:14:54 +0200

    add nbt read/write events

---
+++ API/src/main/java/org/inventivetalent/npclib/event/NPCNBTEvent.java
@@ -0,0 +1,24 @@
+package org.inventivetalent.npclib.event;
+
+import org.inventivetalent.nbt.NBTTag;
+import org.inventivetalent.npclib.npc.NPCAbstract;
+
+public abstract class NPCNBTEvent extends NPCEvent {
+
+	private final Object nbtTagHandle;
+	private final NBTTag nbtTag;
+
+	public NPCNBTEvent(NPCAbstract npc, Object nbtTagHandle, NBTTag nbtTag) {
+		super(npc);
+		this.nbtTagHandle = nbtTagHandle;
+		this.nbtTag = nbtTag;
+	}
+
+	public Object getNbtTagHandle() {
+		return nbtTagHandle;
+	}
+
+	public NBTTag getNbtTag() {
+		return nbtTag;
+	}
+}
