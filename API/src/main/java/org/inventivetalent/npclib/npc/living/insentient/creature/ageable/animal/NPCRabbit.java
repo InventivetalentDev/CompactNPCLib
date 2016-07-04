63130c8fe2b6c9a812bd672da1011990543a4dfb
inventivetalent <inventivetalent@inventivegames.de>
2016-07-04 13:29:19 +0200

    fix rabbit

--- API/src/main/java/org/inventivetalent/npclib/npc/living/insentient/creature/ageable/animal/NPCRabbit.java
+++ API/src/main/java/org/inventivetalent/npclib/npc/living/insentient/creature/ageable/animal/NPCRabbit.java
@@ -1,17 +1,15 @@
 package org.inventivetalent.npclib.npc.living.insentient.creature.ageable.animal;
 
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Horse;
 import org.bukkit.entity.Rabbit;
 import org.inventivetalent.npclib.annotation.NPC;
-import org.inventivetalent.npclib.entity.living.insentient.creature.ageable.animal.EntityHorse;
 import org.inventivetalent.npclib.entity.living.insentient.creature.ageable.animal.EntityRabbit;
 
 @NPC(id = 101,
-	 type = EntityType.HORSE,
-	 bukkit = Horse.class,
+	 type = EntityType.RABBIT,
+	 bukkit = Rabbit.class,
 	 nms = "EntityRabbit",
-	 entity = EntityHorse.class)
+	 entity = EntityRabbit.class)
 public class NPCRabbit extends NPCAnimalAbstract<EntityRabbit, Rabbit> {
 	protected NPCRabbit(EntityRabbit npcEntity) {
 		super(npcEntity);
