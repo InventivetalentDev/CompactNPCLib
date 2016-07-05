3f6575bad67089862a3628d308c69414284bfa90
inventivetalent <inventivetalent@inventivegames.de>
2016-07-05 10:01:33 +0200

    move #getProfile to EntityHuman

--- API/src/main/java/org/inventivetalent/npclib/entity/living/human/NPCEntityHuman.java
+++ API/src/main/java/org/inventivetalent/npclib/entity/living/human/NPCEntityHuman.java
@@ -1,7 +1,12 @@
 package org.inventivetalent.npclib.entity.living.human;
 
+import com.mojang.authlib.GameProfile;
 import org.inventivetalent.npclib.entity.living.NPCEntityLiving;
 
 public interface NPCEntityHuman extends NPCEntityLiving {
+
+	GameProfile getProfile();
+
 	void checkMovement(double x, double y, double z);
+
 }
