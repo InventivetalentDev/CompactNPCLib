696f11bbabf7a6545840bf32f26ea1a8f4696b00
inventivetalent <inventivetalent@inventivegames.de>
2016-07-05 10:08:39 +0200

    add SkinLayer

---
+++ API/src/main/java/org/inventivetalent/npclib/SkinLayer.java
@@ -0,0 +1,29 @@
+package org.inventivetalent.npclib;
+
+public enum SkinLayer {
+
+	CAPE(0),
+	JACKET(1),
+	LEFT_SLEEVE(2),
+	RIGHT_SLEEVE(3),
+	LEFT_PANTS_LEG(4),
+	RIGHT_PANTS_LEG(5),
+	HAT(6);
+
+	private final int id;
+	private final int shifted;
+
+	private SkinLayer(int id) {
+		this.id = id;
+		this.shifted = 1 << id;
+	}
+
+	public int getID() {
+		return this.id;
+	}
+
+	public int getShifted() {
+		return this.shifted;
+	}
+
+}
