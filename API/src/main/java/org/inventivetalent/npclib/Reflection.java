4d0bfa16df15cb9fd7a63665fe4e1a1dccde4ad3
inventivetalent <inventivetalent@inventivegames.de>
2016-07-07 21:34:00 +0200

    remove old code&test

--- API/src/main/java/org/inventivetalent/npclib/Reflection.java
+++ API/src/main/java/org/inventivetalent/npclib/Reflection.java
@@ -5,16 +5,13 @@ import org.bukkit.entity.Entity;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.inventivetalent.reflection.minecraft.Minecraft;
 import org.inventivetalent.reflection.resolver.ClassResolver;
-import org.inventivetalent.reflection.resolver.ConstructorResolver;
 import org.inventivetalent.reflection.resolver.MethodResolver;
 import org.inventivetalent.reflection.resolver.ResolverQuery;
 import org.inventivetalent.reflection.resolver.minecraft.NMSClassResolver;
 import org.inventivetalent.reflection.resolver.minecraft.OBCClassResolver;
-import org.inventivetalent.reflection.resolver.wrapper.ClassWrapper;
 import org.inventivetalent.reflection.resolver.wrapper.MethodWrapper;
 import org.inventivetalent.reflection.util.AccessUtil;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
@@ -27,52 +24,6 @@ public class Reflection {
 	public static final ClassResolver    classResolver    = new ClassResolver();
 	public static final NMSClassResolver nmsClassResolver = new NMSClassResolver();
 	public static final OBCClassResolver obcClassResolver = new OBCClassResolver();
-
-	public static <T> T newInstance(String packageName, String className, Class<? extends T> type, Object... args) {
-		ClassWrapper classWrapper = getVersionedClass(packageName, className);
-		Constructor constructor = new ConstructorResolver(classWrapper.getClazz()).resolveFirstConstructorSilent();
-		try {
-			//noinspection unchecked
-			return (T) constructor.newInstance(args);
-		} catch (ReflectiveOperationException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	/**
-	 * Get the matching class for the current Minecraft version
-	 *
-	 * @param packageName package
-	 * @param className   class
-	 * @return the matching class
-	 */
-	public static ClassWrapper getVersionedClass(String packageName, String className) {
-		return classResolver.resolveWrapper(packageName + "." + className + "_" + Minecraft.VERSION.name());
-	}
-
-	public static <T> T newLazyInstance(String packageName, String className, Class<? extends T> type, Object args) {
-		ClassWrapper classWrapper = getLazyVersionedClass(packageName, className);
-		Constructor constructor = new ConstructorResolver(classWrapper.getClazz()).resolveFirstConstructorSilent();
-		try {
-			//noinspection unchecked
-			return (T) constructor.newInstance(args);
-		} catch (ReflectiveOperationException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	/**
-	 * Get the lazy (without release number) matching class for the current Minecraft version
-	 *
-	 * @param packageName package
-	 * @param className   class
-	 * @return the matching class
-	 */
-	public static ClassWrapper getLazyVersionedClass(String packageName, String className) {
-		String version = Minecraft.VERSION.name();
-		String lazyVersion = version.substring(0, version.lastIndexOf("_R"));// Remove _R?
-		return classResolver.resolveWrapper(packageName + "." + className + "_" + lazyVersion);
-	}
 
 	public static boolean doesClassExist(String name) {
 		try {
--- API/src/test/java/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-
-	public static void main(String... args) {
-		String version = "v1_8_R2";
-		String lazyVersion = version.substring(0, version.lastIndexOf("_R"));// Remove _R?
-		System.out.println(lazyVersion);
-	}
-
-}
