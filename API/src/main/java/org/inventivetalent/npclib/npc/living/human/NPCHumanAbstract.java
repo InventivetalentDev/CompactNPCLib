1054fc203a8e0bec67458bf51e2992a45df9bf71
inventivetalent <inventivetalent@inventivegames.de>
2016-07-09 20:15:54 +0200

    add laying methods

--- API/src/main/java/org/inventivetalent/npclib/npc/living/human/NPCHumanAbstract.java
+++ API/src/main/java/org/inventivetalent/npclib/npc/living/human/NPCHumanAbstract.java
@@ -1,7 +1,11 @@
 package org.inventivetalent.npclib.npc.living.human;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 import org.inventivetalent.nbt.annotation.NBT;
+import org.inventivetalent.npclib.ClassBuilder;
 import org.inventivetalent.npclib.Reflection;
 import org.inventivetalent.npclib.entity.living.human.NPCEntityHuman;
 import org.inventivetalent.npclib.npc.living.NPCLivingAbstract;
@@ -10,6 +14,13 @@ import org.inventivetalent.npclib.skin.SkinLayer;
 import org.inventivetalent.reflection.minecraft.DataWatcher;
 
 public abstract class NPCHumanAbstract<N extends NPCEntityHuman, B extends HumanEntity> extends NPCLivingAbstract<N, B> implements ISkinnableEntity {
+
+	@NBT(value = {
+			"npclib.options",
+			"human",
+			"laying" },
+		 read = false)
+	private boolean laying;
 
 	public NPCHumanAbstract(N npcEntity) {
 		super(npcEntity);
@@ -41,6 +52,33 @@ public abstract class NPCHumanAbstract<N extends NPCEntityHuman, B extends Human
 			return (byte) DataWatcher.getValue(Reflection.getDataWatcher(getBukkitEntity()), 10, DataWatcher.V1_9.ValueType.ENTITY_HUMAN_SKIN_LAYERS);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
+		}
+	}
+
+	@NBT(value = {
+			"npclib.options",
+			"human",
+			"laying" })
+	public void setLaying(@NBT boolean laying) {
+		this.laying = laying;
+		updateNearby();
+	}
+
+	public boolean isLaying() {
+		return laying;
+	}
+
+	@Override
+	public void updateToPlayer(Player player) {
+		if (getBukkitEntity().isDead()) { return; }
+		super.updateToPlayer(player);
+		if (isLaying()) {
+			Location bedLocation = getBukkitEntity().getLocation().clone();
+			bedLocation.setY(0);
+			// Create a fake bed block
+			player.sendBlockChange(bedLocation, Material.BED_BLOCK, (byte) 0);
+
+			sendPacket(player, ClassBuilder.buildPacketPlayOutBed(getBukkitEntity().getEntityId(), bedLocation.getBlockX(), bedLocation.getBlockY(), bedLocation.getBlockZ()));
 		}
 	}
 
