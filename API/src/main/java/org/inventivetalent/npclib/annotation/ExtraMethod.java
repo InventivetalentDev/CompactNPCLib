74a82e4ca22b971125803b8d226e6bc63da4dccf
inventivetalent <inventivetalent@inventivegames.de>
2017-03-05 09:29:50 +0100

    add options to @ExtraMethod to specify the versions to generate for

--- API/src/main/java/org/inventivetalent/npclib/annotation/ExtraMethod.java
+++ API/src/main/java/org/inventivetalent/npclib/annotation/ExtraMethod.java
@@ -1,5 +1,7 @@
 package org.inventivetalent.npclib.annotation;
 
+import org.inventivetalent.reflection.minecraft.Minecraft;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,5 +10,25 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExtraMethod {
+
 	String value();
+
+	/**
+	 * If specified, the annotated method will be generate for this version upwards
+	 * @return the Version
+	 */
+	Minecraft.Version fromVersion() default Minecraft.Version.UNKNOWN;
+
+	/**
+	 * If specified, the annotated method will be generate for this version downwards
+	 * @return the Version
+	 */
+	Minecraft.Version untilVersion() default Minecraft.Version.UNKNOWN;
+
+	/**
+	 * If specified, the annotated method will be generate for only these versions
+	 * @return the Version
+	 */
+	Minecraft.Version[] forVersion() default {};
+
 }
