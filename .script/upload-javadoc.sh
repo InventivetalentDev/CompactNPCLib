e82cbe87cf4b9608c1758671d293c48df27012d1
Haylee Schäfer <mail@inventivetalent.org>
2017-04-13 20:00:44 +0200

    Update upload-javadoc.sh

--- .script/upload-javadoc.sh
+++ .script/upload-javadoc.sh
@@ -6,7 +6,7 @@ if [ -n "$GITHUB_TOKEN" ]; then
     git checkout -b gh-pages || git checkout gh-pages
     git add .
     git -c user.name='InventiveBot' -c user.email='git-bot@inventivetalent.org' 
-    git commit -m Update Javadoc
+    git commit -m "Update Javadoc"
     # Make sure to make the output quiet, or else the API token will leak!
     # This works because the API key can replace your password.
     git push -f -q https://InventiveBot:$GITHUB_TOKEN@github.com/InventivetalentDev/CompactNPCLibs gh-pages &2>/dev/null
