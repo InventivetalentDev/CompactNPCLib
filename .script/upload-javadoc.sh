6f0cb3e7bc118d0d40dbc22589f4201be623a42c
Haylee Schäfer <mail@inventivetalent.org>
2017-04-13 20:51:34 +0200

    Update upload-javadoc.sh

--- .script/upload-javadoc.sh
+++ .script/upload-javadoc.sh
@@ -1,15 +1,17 @@
-if [ -n "$GITHUB_TOKEN" ]; then
+#!/bin/sh
+if [ -n "$GITHUB_API_KEY" ]; then
     cd "$TRAVIS_BUILD_DIR"
     mkdir docs
     cd docs
     git init
-    git checkout -b gh-pages || git checkout gh-pages
+    git checkout -b gh-pages || git checkout gh-pageswd
+    cp "$TRAVIS_BUILD_DIR/target/site/apidocs" "."
     git add .
     git config --global user.email "git-bot@inventivetalent.org"
     git config --global user.name "InventiveBot"
     git commit -m "Update Javadoc"
     # Make sure to make the output quiet, or else the API token will leak!
     # This works because the API key can replace your password.
-    git push -f -q https://InventiveBot:$GITHUB_TOKEN@github.com/InventivetalentDev/CompactNPCLib gh-pages &2>/dev/null
+    git push -f -q https://InventiveBot:$GITHUB_API_KEY@github.com/InventivetalentDev/CompactNPCLibs gh-pages &2>/dev/null
     cd "$TRAVIS_BUILD_DIR"
-  fi
+endif
