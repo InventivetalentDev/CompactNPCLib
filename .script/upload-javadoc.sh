04b9cdab11c91ffbaff881908e1b804714f441c7
Haylee Schäfer <mail@inventivetalent.org>
2017-04-14 10:52:05 +0200

    Update upload-javadoc.sh

--- .script/upload-javadoc.sh
+++ .script/upload-javadoc.sh
@@ -2,13 +2,13 @@
 echo -e "Uploading Javadoc...\n"
 
 cd $HOME
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "travis-ci"
+git config --global user.email "git-bot@inventivetalent.org"
+git config --global user.name "InventiveBot"
 git clone --quiet --branch=gh-pages https://${GITHUB_TOKEN}@github.com/InventivetalentDev/CompactNPCLib gh-pages > /dev/null
 
 cd gh-pages
-git rm -rf ./doc
-cp -Rf $HOME/target/site ./doc
+git rm -rf .
+cp -Rf $HOME/target/site .
 git add -f .
 git commit -m "Javadoc for build $TRAVIS_BUILD_NUMBER"
 git push -fq origin gh-pages > /dev/null
