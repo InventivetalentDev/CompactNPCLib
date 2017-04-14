92cbc6f2b38a7769973535349b9e5496f665619e
Haylee Schäfer <mail@inventivetalent.org>
2017-04-14 10:58:30 +0200

    Update upload-javadoc.sh

--- .script/upload-javadoc.sh
+++ .script/upload-javadoc.sh
@@ -8,7 +8,7 @@ git clone --quiet --branch=gh-pages https://${GITHUB_TOKEN}@github.com/Inventive
 
 cd gh-pages
 git rm -rf .
-cp -Rf $TRAVIS_BUILD_DIR/target/site .
+cp -Rf $TRAVIS_BUILD_DIR/target/site/apidocs .
 git add -f .
 git commit -m "Javadoc for build $TRAVIS_BUILD_NUMBER"
 git push -fq origin gh-pages > /dev/null
